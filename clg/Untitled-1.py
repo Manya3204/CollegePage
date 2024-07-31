b8f86d538ed1ea46e7228419558a095a25d77a8e
Manya3204 <138611558+Manya3204@users.noreply.github.com>
2024-07-31 21:46:14 +0530

    Update Untitled-1.py

--- clg/Untitled-1.py
+++ clg/Untitled-1.py
@@ -1,17 +1,1 @@
-def getMinMax(a, n):
-    maxm = a[0]
-    for i in range(1, n):
-        if (a[i] > maxm):
-            maxm = a[i]
-        return a[i]
-        
-    minm = a[0]
-    for j in range(1, n):
-        if (a[j] < minm):
-            minm = a[j]
-        return a[j]
-        
-a = [3,2,1,56,1000,167]
-n=6
-res = getMinMax(a, n)
-print(res)
+
